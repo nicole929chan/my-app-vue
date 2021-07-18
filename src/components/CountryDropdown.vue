c37f3f673509ea249cb569bac71cfcdf0d52daef
Nicole <nicole929chan@gmail.com>
2021-07-18 23:19:56 +0800

    template refs

---
+++ src/components/CountryDropdown.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>country(data) 是一個 ref 物件</div>
+  <select @change="changed" ref="country">
+    <option value="">Please choose</option>
+    <option value="UK">United Kingdom</option>
+    <option value="US">America</option>
+    <option value="In">India</option>
+  </select>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      country: ''
+    }
+  },
+  mounted() {
+    // 整個<select>...</select> DOM元素
+    console.log(this.$refs.country);
+  },
+  methods: {
+    changed() {
+      // select DOM 元素所選中的值
+      this.$emit('changed', this.$refs.country.value)
+    }
+  }
+}
+</script>
---
+++ src/components/Register.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Register...</h1>
+  <CountryDropdown @changed="changeCountry" />
+</template>
+
+<script>
+import CountryDropdown from './CountryDropdown.vue'
+
+export default {
+  components: {
+    CountryDropdown
+  },
+  data() {
+    return {
+      form: {
+        name: '',
+        country: ''
+      }
+    }
+  },
+  methods: {
+    changeCountry(value) {
+      // value is from CountryDropdown component
+      this.form.country = value
+    }
+  }
+}
+</script>
